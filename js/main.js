f9bf86c55362b5cc69c82d27599b9964655b7c64
LECTEUR16 <kamolaazimova45@gmail.com>
2023-06-05 08:25:44 +0500

    initial

---
+++ js/main.js
@@ -0,0 +1,8 @@
+alert('salom')
+var num1 = prompt('number1')
+var num2 = prompt('number2')
+
+console.log(num1*1 + num2*1);
+console.log(num1 / 'num2');
+console.log(num1 * 'num2');
+console.log(num1 - 'num2');
